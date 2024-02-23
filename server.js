c581da43a7f1ced0616a84d2c92cbee0a789dae0
Emma Roizot <emma.roizot@orange.fr>
2024-02-23 14:31:35 +0100

    ajout de nodemon

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res, next) => {
-    res.send('<h1>Bonjour !</h1>');
+    res.send("<h1>Bien le bonjour tout le monde :)</h1><img src='https://i.pinimg.com/736x/6f/74/c5/6f74c58a3841a6cd6b1477835d759051.jpg' alt='Image Pin' width='300px'>");
 }
 );
