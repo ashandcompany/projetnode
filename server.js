e30110da5bf2d0ff4cfa8821414109faf6cbc84e
Emma Roizot <emma.roizot@orange.fr>
2024-02-23 14:19:38 +0100

    ajout du serveur

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+let port = 3000;
+
+app.listen(port, () => {
+    console.log('Leserveurestenroute');
+    console.log(`Serveurlisteningathttp://localhost:${port}`);
+})
+
+app.get('/', (req, res, next) => {
+    res.send('Bonjour !');
+}
+);
