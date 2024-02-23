3a074e97e49434facc42adfb86e57cd00bca874b
Emma Roizot <emma.roizot@orange.fr>
2024-02-23 15:33:12 +0100

    amélioration du menu

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let port = 3000;
 app.set('view engine', 'ejs');
 app.use('/views', express.static(__dirname + '/views')); // redirect views
 
-app.use(express.static(__dirname+'/www'));
+app.use(express.static(__dirname + '/www'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
@@ -18,5 +18,12 @@ app.listen(port, () => {
 
 app.get('/', (req, res, next) => {
     res.render('index.ejs');
-}
-);
+});
+
+app.get('/info', (req, res, next) => {
+    res.render('info.ejs');
+});
+
+app.get('/contact', (req, res, next) => {
+    res.render('contact.ejs');
+});
